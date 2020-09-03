00fa8d97cccf9c4e08177468e53f89f7ca72a660
Oscar A Castro <oacastro2@gmail.com>
2020-09-03 22:53:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,21 @@
 
 def key_for_min_value(name_hash)
    
-
+   min_key = nil  
+  min_value = nil 
+  name_hash.each do | key, value | 
+    if min_value ==  nil  
+      min_value = value
+      min_key = key    
+    elsif value < min_value
+      min_value = value  
+      min_key = key 
+    end 
+        
+       
+  end 
+puts min_key 
 end
-    
+key_for_min_value(hash)
 
-end
+
